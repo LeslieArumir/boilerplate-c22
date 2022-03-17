56037712d18c4bc56c8c3b0cd01c94bdb02204fc
LeslieArumir <89419362+LeslieArumir@users.noreply.github.com>
2022-03-17 15:25:53 -0600

    Add files via upload

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
-# boilerplate-c22
-Clase 22 coding
+# p5.play-boilerplate
+Boiler plate for p5.play
---
+++ sketch.js
@@ -0,0 +1,9 @@
+function setup() {
+  createCanvas(400,400);
+  createSprite(200, 200, 50, 50);
+}
+
+function draw() {
+  background(0,0,0);  
+  drawSprites();
+}
